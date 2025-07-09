c13a44ae49ff59cf8fc4ab0f4c69e119f541bc34
arezki <arezki.guezout@gmail.com>
2025-07-09 20:25:36 +0100

    c2

--- demo/src/test/java/com/saucedemo/Hooks/Hook.java
+++ demo/src/test/java/com/saucedemo/Hooks/Hook.java
@@ -6,10 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import com.saucedemo.Tools.Wdm;
 
 public class Hook {
+
     @BeforeEach
     public void setup(){
         Wdm.createdriver();
+        Wdm.getDriver().get("https://www.saucedemo.com/");    
     }
+
+
     @AfterEach
     public void teardown(){
         Wdm.destroydriver();
